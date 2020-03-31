11a4c52acc605812328f4a51ee233778710dd738
starter-blog <qkdwl462@cau.ac.kr>
2020-04-01 00:26:34 +0900

    node

--- js/base.js
+++ js/base.js
@@ -2,8 +2,7 @@ var gridContainers = document.getElementsByClassName("grid-container");
 for (var i = 0; i < gridContainers.length; i++) {
 	var gridContainer = gridContainers[i];
 	var childElementCount = gridContainer.childElementCount;
-	var columns = "";
 	const colcnt = Math.sqrt(childElementCount);
-	for (var j = 0; j < colcnt; j++) columns += 100 / colcnt + "% ";
+	const columns = "repeat(" + Math.round(colcnt) + ", 1fr)";
 	gridContainer.style.gridTemplateColumns = columns;
 }
